21f5d3da9cb83def889897e27e3c1289a0fb3780
ashwin <ashwin.achu.1311@gmail.com>
2018-09-17 23:41:53 +0530

    heroku changes6

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/trutimeapplication/src'));
+app.use(express.static(__dirname + '/dist/src/trutimeapplication'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/trutimeapplication/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/src/trutimeapplication/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
