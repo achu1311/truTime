0a8c3c794992df3c85aff23cd8023eea99007f04
ashwin <ashwin.achu.1311@gmail.com>
2018-09-17 23:27:11 +0530

    heroku changes3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('/dist/trutimeapplication'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/trutimeapplication/index.html'));
+res.sendFile(path.join(__dirname+'/dist/trutimeapplication/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
