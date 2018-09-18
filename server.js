66ff4c2c8a2291775732169c61ccd6bb771f25fe
ashwin <ashwin.achu.1311@gmail.com>
2018-09-18 23:12:04 +0530

    first3

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/truTime'));
+app.use(express.static('/app/dist/truTimeCal'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/truTime/index.html'));
+res.sendFile(path.join('/app/dist/truTimeCal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
