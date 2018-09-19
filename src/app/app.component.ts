e35160de2491c3a43df763f9c2eb07a568ceb571
Ashwin Balaji <671976@cognizant.com>
2018-09-19 16:07:27 +0530

    changes for handling minutes and hours in count<5 V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,14 +44,19 @@ export class AppComponent {
       this.addTime(this.number5);
       // console.log(this.number5);
     }
-    this.hours += Math.floor(this.minutes / 60);
-    this.minutes = this.minutes % 60;
-    console.log('time', this.hours, '  ', this.minutes);
+    // console.log(this.minutes/60);
+    this.hours+=Math.floor(this.minutes/60);
+   
+    this.minutes = parseInt((this.minutes % 60).toFixed(0));
+    let completedMinutes=this.minutes;
+    console.log('time', this.hours, '  ', this.minutes.toFixed(0));
 
     this.split_time = (45 - (this.hours));
     console.log('time2', this.split_time, ' ', this.hours, '  ', this.minutes);
     if (this.minutes > 0) {
-      this.split_time -= (0.60 - (this.minutes / 100));
+      this.minutes=60-this.minutes;
+      //cutrrent 
+      // this.split_time -= (0.60 - (this.minutes / 100));
       console.log('time3', this.split_time, ' ', this.hours, '  ', this.minutes);
     }
     console.log('finaltime', this.split_time, 'count', this.count);
@@ -78,8 +83,61 @@ export class AppComponent {
     else if (this.count > 0) {
       // this.count=;
       console.log('before this.split final',this.split_time);
-      this.split_time=parseFloat((this.split_time / (5 - this.count)).toFixed(2))
-      this.time = 'please stay for ' + (this.split_time );
+ //current
+      // let temp=100-((this.split_time % 1)*100);
+      let temp=this.minutes;
+      console.log('temp',temp);
+      let totalWorkingHours;
+      let stayingMinutes,stayingHours;
+      if(temp>=0){
+        totalWorkingHours=45-this.split_time+(completedMinutes/100)
+      }
+      console.log('total working hours',totalWorkingHours);
+      if(totalWorkingHours===(9*this.count)){
+        console.log('exact',this.split_time,' ',this.minutes,' ',completedMinutes);
+        stayingHours=(this.split_time)/(5-this.count);
+        stayingMinutes=0;
+        console.log('staying minutes',stayingHours);
+      }
+      if(totalWorkingHours>(9*this.count)){
+        console.log('worked more',this.split_time,' ',this.minutes,' ',completedMinutes);
+        stayingHours=(this.split_time-1)/(5-this.count);
+        if(completedMinutes>0)
+        stayingMinutes=60-(completedMinutes/(5-this.count));
+        console.log('staying minutes',stayingHours,' ',stayingMinutes);
+
+      }
+      else{
+        
+        console.log('less',this.split_time,' ',this.minutes,' ',completedMinutes);
+        stayingHours=(this.split_time-1)/(5-this.count);
+        if(this.minutes>0)
+        stayingMinutes=this.minutes/(5-this.count);
+        console.log('staying minutes',stayingHours,' ',stayingMinutes);
+      }
+      // if(temp<60){
+        //current
+        // this.split_time=Math.floor(this.split_time)+(temp/100)-Math.floor((this.split_time % 1))
+      // }
+     
+     
+      // console.log('temp2',this.split_time,' ',this.minutes,' ',completedMinutes);
+      
+      // this.split_time=parseFloat((this.split_time-(this.minutes/100)).toFixed(2))
+      // if(completedMinutes>0)
+      // this.split_time=(this.split_time-1)/(5-this.count);
+      
+      // this.minutes=60-((completedMinutes)/(5-this.count));
+      // console.log('before this.split final2',this.split_time,' ',this.minutes);
+
+      // this.split_time=parseFloat((this.split_time / (5 - this.count)).toFixed(2))
+      
+      
+
+      // console.log('before this.split final3',this.split_time);
+
+      // this.time = 'please stay for ' + (this.split_time[0] )+' '+this.split_time[1];
+            this.time = 'Please maintain average of ' + Math.floor(stayingHours)+' Hours '+Math.floor(stayingMinutes)+' Minutes';
     }
 
 
@@ -93,13 +151,16 @@ export class AppComponent {
       this.split_time =
         [
           (number > 0) ? Math.floor(number) : Math.ceil(number),
-          Math.floor((number % 1) * 100)
+          (number % 1) * 100
         ]
       console.log('split_time', this.split_time);
       this.hours += this.split_time[0];
       console.log('hours', this.hours);
       if (this.split_time[1] > 0)
-        this.minutes += this.split_time[1];
+      {
+        this.minutes += parseInt(this.split_time[1].toFixed(0))
+      }
+        
       console.log('minutes', this.minutes);
     }
   
